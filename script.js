9f4d742d3dbf848980cd0b0045308aa6f2434a11
eduardogm25 <47434972+eduardogm25@users.noreply.github.com>
2019-03-21 14:50:26 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+//Dissapear Cartera
+
+$("#button1").click(function() {
+    $("#wallet").toggle();
+});
+
+
+
+//Disintegrate
+
+$("#button2").click(function() {
+    $("#keys").fadeToggle();
+});
+
+
+
+//Smash Iphone
+
+$("#button3").click(function() {
+    $("#iphone").slideToggle();
+});
